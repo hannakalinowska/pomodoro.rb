06cbade5e005ddc5c9bc9b3c79c197970c5abd82
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-07-29 21:19:55 +0100

    renames and cleanup

--- pomodoro.rb
+++ pomodoro.rb
@@ -7,31 +7,31 @@ pomodoro = OpenStruct.new(:name => 'Pomodoro', :time => 25 * 60, :message => 'Po
 short_break = OpenStruct.new(:name => 'Short break', :time => 5 * 60, :message => 'Pomodoro Break is up!', :notifier => notifier)
 long_break = OpenStruct.new(:name => 'Long break', :time => 15 * 60, :message => 'Pomodoro Break is up!', :notifier => notifier)
 
-def say_start
+def start(chunk)
+  puts "\n#{chunk.name}!"
   puts "started: #{Time.now.strftime('%H:%M')}"
 end
 
-def progress(time, number_of_chunks)
-  chunk = 1.0 * time / number_of_chunks
-  1.upto(number_of_chunks) do |i|
+def progress(time, number_of_updates)
+  duration = 1.0 * time / number_of_updates
+  1.upto(number_of_updates) do |i|
     print "|"
     print '==' * i
-    print '  ' * (number_of_chunks - i)
+    print '  ' * (number_of_updates - i)
     print "|\r"
     STDOUT.flush
-    sleep chunk
+    sleep duration
   end
 end
 
-def notify_about(chunk)
+def finish(chunk)
   `#{chunk.notifier} #{chunk.message}`
 end
 
 def run(chunk)
-  puts "\n#{chunk.name}!"
-  say_start
+  start(chunk)
   progress(chunk.time, 20)
-  notify_about(chunk)
+  finish(chunk)
 end
 
 pomodoro_count = 0
