6f38918ca2e8c8263e775abc34a1ffdc47631cf6
Hanna Kalinowska <h.kalinowska@nature.com>
2011-07-29 10:27:37 +0100

    with a progress bar

--- pomodoro.rb
+++ pomodoro.rb
@@ -2,22 +2,34 @@
 
 require 'ostruct'
 
-pomodoro = OpenStruct.new(:time => 4, :message => 'Pomodoro Time is up!')
-pause = OpenStruct.new(:time => 2, :message => 'Pomodoro Break is up!')
+pomodoro = OpenStruct.new(:time => 1500, :message => 'Pomodoro Time is up!')
+pause = OpenStruct.new(:time => 300, :message => 'Pomodoro Break is up!')
 notifier = 'growlnotify -s -m '
 
 def say_start
   puts "started: #{Time.now.strftime('%H:%M')}"
 end
 
+def progress(time, number_of_chunks)
+  chunk = 1.0 * time / number_of_chunks
+  1.upto(number_of_chunks) do |i|
+    print "|"
+    print '=' * i
+    print ' ' * (number_of_chunks - i)
+    print "|\r"
+    STDOUT.flush
+    sleep chunk
+  end
+end
+
 loop do
-  puts 'Pomodoro!'
+  puts "\nPomodoro!"
   say_start
-  sleep pomodoro.time
+  progress(pomodoro.time, 20)
   `#{notifier} #{pomodoro.message}`
 
-  puts 'Break!'
+  puts "\nBreak!"
   say_start
-  sleep pause.time
+  progress(pomodoro.time, 20)
   `#{notifier} #{pause.message}`
 end
